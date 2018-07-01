b7ed0d93f99c666555278dd3e486c9856eb27ee0
Ray Li <ray.li.verified@gmail.com>
2018-07-01 12:51:03 -0500

    Update AndroidX
    *Update to AndroidX.
    *Update Gradle to v3.2.0-beta02.
    *TargetSDK v28.

--- app/src/main/java/stream/custompermissionsapp/MainActivity.java
+++ app/src/main/java/stream/custompermissionsapp/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 
